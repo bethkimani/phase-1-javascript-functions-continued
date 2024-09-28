d5ab143e47666ab5899009f84aef3cb02f0bc153
Beth Kimani <kbwaceke@gmail.com>
2024-09-28 08:43:08 +0300

    Add function continue

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+
+console.log(saturdayFun());              
+console.log(saturdayFun('go hiking'));   
+console.log(mondayWork());               
+console.log(mondayWork('work from home'));
+
+
+
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
+
+const wrapWithStar = wrapAdjective("*");
+console.log(wrapWithStar("a hard worker"));  // Output: You are *a hard worker*!
+
+const wrapWithPipe = wrapAdjective("||");
+console.log(wrapWithPipe("dedicated"));      // Output: You are ||dedicated||!
+
